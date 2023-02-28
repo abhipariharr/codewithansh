689c0dc7b69ca387c0ba0decfbca58df28fc58cd
AnshVerma04 <126587833+AnshVerma04@users.noreply.github.com>
2023-02-28 20:04:24 +0530

    Add files via upload

---
+++ Averagebyuser.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("Enter the first number=");
+    scanf("%d",&a);
+    printf("Enter the second number=");
+    scanf("%d",&b);
+    printf("Enetr the third number=");
+    scanf("%d",&c);
+    printf("Enter the fourth number=");
+    scanf("%d",&d);
+    printf("Average of four number=%d",(a+b+c+d)/4);
+    return 0;
+     
+}
